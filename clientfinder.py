bee74353dcc8596860048046866d39f01e77138f
Thie Thomsen <powertieke@gmail.com>
2013-08-27 17:21:00 +0200

    flopsa

--- clientfinder.py
+++ clientfinder.py
@@ -20,7 +20,7 @@ def make_control_socket(socketdict, discovered, port):
 		clientSocket.sendall('status'.encode('UTF-8'))
 		answer = clientSocket.recv(1024).decode('UTF-8')
 		socketdict[client[0]] = [clientSocket, answer]
-		print socketdict
+		print(socketdict)
 
 class ClientFinderThread(threading.Thread):
 	def __init__(self, discovered, port, name):
