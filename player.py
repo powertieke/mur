38bbd7435697561fa795ca1d571a104f22a9ce89
Thie Thomsen <powertieke@gmail.com>
2013-12-11 08:12:18 +0100

    Remove console messages

--- player.py
+++ player.py
@@ -17,10 +17,10 @@ messagequeue = queue.Queue()
 def set_background(color):
 	subprocess.call('sudo sh -c "TERM=linux setterm -background ' + color + ' >/dev/tty0"', shell=True)
 	subprocess.call('sudo sh -c "TERM=linux setterm -clear >/dev/tty0"', shell=True)
-	subprocess.call('sudo cat image.raw > /dev/fb0 2> /dev.null', shell=True)
+	subprocess.call('sudo cat image.raw > /dev/fb0 2>> /home/pi/mur.log', shell=True)
 
 def kill_all_omxplayers():
-	subprocess.call("sudo killall omxplayer omxplayer.bin 2> /dev/null", shell=True)
+	subprocess.call("sudo killall omxplayer omxplayer.bin 2>>  /home/pi/mur.log", shell=True)
 
 def ready_player(moviefile, stopqueue, duration):
 	player = pyomxplayer.OMXPlayer('"' + moviefile + '"', stopqueue, duration, None, True)
