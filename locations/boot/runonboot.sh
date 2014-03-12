65a6582f4de448ffee963537e39c184cc0fc117d
Thie Thomsen <powertieke@gmail.com>
2014-03-12 12:03:01 +0100

    Started work on webinterface

---
+++ locations/boot/runonboot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -f /boot/update_now ];
+	then
+	cd /home/pi/mur
+	git pull
+	cd /boot
+	rm update_now
+	reboot
+fi
+
+if [ -e /dev/sda1 ];
+	then
+	mount -t vfat /dev/sda1 /media/usb
+	cd /home/pi/mur
+	python3 muur.py -s pi4 /media/usb/
+fi
