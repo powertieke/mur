7543f0f251bd0bc88de2a26497d3a064b462c7ca
Thie Thomsen <powertieke@gmail.com>
2014-03-21 10:27:59 +0100

    Breaking the silence of the night, can you hear me screeeeeeaming

--- player.py
+++ player.py
@@ -178,7 +178,7 @@ def play_synced_movie(moviefile, incoming_from_controller, outgoing_to_controlle
 	outgoing_to_controller.put("ready") # let the controlling pi know we're ready to go
 	
 	if syncqueue.get() == "go":
-		tolerance = 100000.0
+		tolerance = 50000.0
 		player.toggle_pause() # Play synced movie
 		# print("Got go: playing")
 		while True:
