74b5d8740c78eba67edd8596e71cb08e05f72f51
Thie Thomsen <powertieke@gmail.com>
2014-04-07 11:48:43 +0200

    Breaking the silence of the night, can you hear me screeeeeeaming

--- muur.py
+++ muur.py
@@ -64,7 +64,7 @@ def main():
 	if args.master :
 		foundclients = clientfinder.clientfinder(udpport_discovery, tcpport) # Listens to discovery broadcasts from unconnected pi's in the same network and sets up a control connection over TCP.
 		# interface.interface(foundclients, udpport_sync, args.moviepath)
-		webinterface.webinterface(foundclients, udpport_sync, args.moviepath)
+		interface.interface(foundclients, udpport_sync, args.moviepath)
 		
 	if args.slave :
 		player.set_background('white')
