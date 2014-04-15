05004f46e0d73f40d21f884434b1d3ae3ff0267a
Thie Thomsen <powertieke@gmail.com>
2014-04-15 17:01:26 +0200

    Breaking the silence of the night, can you hear me screeeeeeaming

--- client.py
+++ client.py
@@ -5,12 +5,13 @@ import os
 import socket
 import time
 import threading
+import queue
 
 """This is where the client networking and controller discovery functions live"""
 
 
 
-def listener(port, statport):
+def listener(port, outqueue):
 	"""Listens for incoming TCP connection requests and returns the connection"""
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind(("", port))
@@ -18,14 +19,8 @@ def listener(port, statport):
 	conn, addr = s.accept()
 	conn.recv(1024)
 	conn.sendall('0'.encode('utf-8'))
-	s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s2.bind(("", statport))
-	s2.listen(1)
-	conn2, addr = s2.accept()
-	conn2.recv(1024)
-	conn2.sendall('0'.encode('utf-8'))
-	return conn, conn2
-
+	outqueue.put(conn)
+	
 def screamer(clientname, udpport_discovery):
 	"""Screams it's name through UDP out over the local network in search of a controller pi"""
 	time.sleep(5) # give the main thread a couple of seconds to ready the connection before it starts yelling it's name - Fix better later (using a thread and a queue)
@@ -40,7 +35,16 @@ def screamer(clientname, udpport_discovery):
 		if exitflag == 1:
 			break
 	
+class ListenerThread(threading.Thread):
+	def __init__(self, port, outqueue, name):
+		threading.Thread.__init__(self, name=name)
+		self.clientname = clientname
+		self.port = port
+		self.outqueue = outqueue
+	def run(self):
+		listener(self.port, self.outqueue)
 	
+		
 class ScreamerThread(threading.Thread):
 	"""The tread running the screaming process. Else we could not do anything while the pi is looking for a server."""
 	def __init__(self, clientname, udpport, name):
@@ -55,9 +59,16 @@ def find_controller(clientname, udpport, tcpport, statport):
 	"""Opens a listening TCP socket, and starts yelling around for the controller. Returns a connection with the controller (when found)."""
 	global exitflag
 	exitflag = 0
+	cqueue = queue.Queue()
+	squeue = queue.Queue()
+	clistener = ListenerThread(tcpport, cqueue, "controlportthread")
+	clistener.start()
+	slistener = ListenerThread(statport, squeue,"controlportthread")
+	slistener.start()
 	screamerThread = ScreamerThread(clientname, udpport, "screamer1")
 	screamerThread.start()
-	controlSocket, statsocket = listener(tcpport, statport)
+	controlSocket = cqueue.get()
+	statsocket = squeue.get()
 	exitflag = 1
 	return controlSocket, statsocket
 
