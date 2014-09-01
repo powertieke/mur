d4c4c642b4d4744c647276d1e9e16860fcd7a4aa
Thie Thomsen <powertieke@gmail.com>
2014-09-01 08:30:44 +0200

    Alles stuk. Alles. Dbus it is\!

--- install/afterreboot.sh
+++ install/afterreboot.sh
@@ -5,3 +5,7 @@ apt-get install omxplayer -y
 apt-get install python3-dbus -y
 apt-get install python-dbus -y
 apt-get install lighttpd -y
+cp ../location/boot/runonboot.sh /boot/
+cp ../location/etc/lighttpd /etc/lighttpd
+cp ../location/etc/rc.local /etc/rc.local
+reboot
