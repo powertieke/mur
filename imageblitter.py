e9e9213f862af3b7c654c1a642fda430abd49edc
Thie Thomsen <powertieke@gmail.com>
2014-09-19 11:48:00 +0200

    Alles stuk. Alles. Dbus it is\!

--- imageblitter.py
+++ imageblitter.py
@@ -16,7 +16,7 @@ def main():
 	w = 0
 	h = 0
 	size = (w, h)
-	screen = pygame.display.set_mode(size, FULLSCREEN)
+	screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 	
 	image = pygame.image.load(glob.glob(args.imagefolder + "*.png")[0])
 	screen.blit(image, (0,0))
