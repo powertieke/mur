572c75504c1941223175d6a7efe8e9705b9d2b05
Thie Thomsen <powertieke@gmail.com>
2014-09-24 11:33:12 +0200

    Alles stuk. Alles. Dbus it is\!

--- imageblitter.py
+++ imageblitter.py
@@ -9,7 +9,7 @@ def main():
 	parser = argparse.ArgumentParser(description='utillity for overlaying an image')
 	parser.add_argument('-f', '--fifo', help='Define which fifo is used for communication')
 	parser.add_argument("imagefolder", help='Location of image')
-	
+	parser.add_argument("hostname", help="Hostname of pi")	
 	args = parser.parse_args()
 	
 	pygame.init()
@@ -19,8 +19,14 @@ def main():
 	h = 0
 	size = (w, h)
 	screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+	theImage = glob.glob(args.imagefolder + "*" + args.hostname + ".png")
 	try:
-		image = pygame.image.load(glob.glob(args.imagefolder + "*.png")[0]).convert()
+		if theImage != []:
+			theImage = theImage[0]
+		else:
+			theImage = glob.glob(args.imagefolder + "*.png")[0]
+		
+		image = pygame.image.load(theImage).convert()
 		screen.blit(image, (0,0))
 		pygame.display.flip()
 	except:
