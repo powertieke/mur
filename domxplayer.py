505633f36ca9a2ca2874c296ee4e787448f52b5e
Thie Thomsen <powertieke@gmail.com>
2014-09-25 15:59:26 +0200

    Alles stuk. Alles. Dbus it is\!

--- domxplayer.py
+++ domxplayer.py
@@ -34,7 +34,7 @@ def player_process(parent):
 	## print("DBUSNAME = " + parent.dbusname)
 	try:
 		## retcode = subprocess.call(["/usr/bin/omxplayer", "-o", "hdmi", parent.moviefile, "--dbus_name", parent.dbusname, "--win", '"0 0 1919 1079"', "--no-osd"], stdout=open(os.devnull, 'wb'), shell=False)
-		retcode = subprocess.call(["/usr/bin/omxplayer", parent.moviefile, "--dbus_name", parent.dbusname, "--win", '"0 0 1919 1079"', "--no-osd"], stdout=open(os.devnull, 'wb'), shell=False)
+		retcode = subprocess.call(["/usr/bin/omxplayer", "-o", "local", parent.moviefile, "--dbus_name", parent.dbusname, "--win", '"0 0 1919 1079"', "--no-osd"], stdout=open(os.devnull, 'wb'), shell=False)
 		if retcode != 0:
 			print(retcode)
 	except:
