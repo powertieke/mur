63ccb38bbe2dada07e83032b81162558323be6ab
Thie Thomsen <powertieke@gmail.com>
2016-02-04 09:59:41 +0100

    Added Sudo command to rc.local

--- install/afterreboot.sh
+++ install/afterreboot.sh
@@ -7,6 +7,8 @@ fi
 mv /etc/rc.local.bak /etc/rc.local
 apt-get update -y
 apt-get upgrade -y
+apt-get install python-pygame -y
+apt-get install python3-pygame -y
 apt-get install omxplayer -y
 apt-get install python3-dbus -y
 apt-get install python-dbus -y
