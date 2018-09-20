e11616fb0344bcc3e5ee08fb26086a9dc9f9d170
Thie Thomsen <t.thomsen@ecobliss.com>
2018-09-20 15:13:21 +0200

    Added dbus-x11 to installer

--- install/afterreboot.sh
+++ install/afterreboot.sh
@@ -7,6 +7,7 @@ fi
 mv /etc/rc.local.bak /etc/rc.local
 apt-get update -y
 apt-get upgrade -y
+apt-get install dbus-x11 -y
 apt-get install python-pygame -y
 apt-get install python3-pygame -y
 apt-get install omxplayer -y
